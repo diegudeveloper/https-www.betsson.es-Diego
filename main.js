76e3e6302352e593f13ee68e0f6021ba8d0c7c2d
Diegu <diegudeveloper@gmail.com>
2023-11-03 11:14:21 +0100

    Implemented informational section and cookie management

--- main.js
+++ main.js
@@ -44,20 +44,67 @@ function nextSlide() {
 
 setInterval(nextSlide, 3000); // Cambia de imagen cada 3 segundos
 
+//ACORDION//
+
+document.addEventListener('DOMContentLoaded', function () {
+  const buttons = document.querySelectorAll('.accordion-button');
+
+  buttons.forEach(function (button) {
+      button.addEventListener('click', function () {
+          const content = this.nextElementSibling;
+          if (content.style.maxHeight) {
+              content.style.maxHeight = null;
+              this.classList.remove('active');
+          } else {
+              content.style.maxHeight = content.scrollHeight + 'px';
+              this.classList.add('active');
+          }
+      });
+  });
+});
 
 
+//cookies//
 
-// const slider = document.querySelector(".slider");
+document.addEventListener('DOMContentLoaded', function () {
+  const cookiesPopup = document.getElementById("cookiesPopup");
 
-// let imgs = slider.querySelectorAll("img")
+  // Función para establecer una cookie
+  function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  }
 
-// let index = 1;
+  // Función para aceptar el aviso de cookies y establecer la cookie
+  function acceptCookies() {
+    setCookie("cookiesAccepted", "true", 30); // Establecer la cookie por 30 días
+    cookiesPopup.style.display = "none";
+  }
 
-// setInterval(function() {
-//     let percentage = index * -100;
-//     slider.style.transform = "translateX(" + percentage + "%)";
-//     index++;
-//     if (index > (imgs.length - 1)) {
-//         index = 0;
-//     }
-// }, 3000);
+  // Función para cerrar el aviso de cookies sin establecer la cookie
+  function closeCookies() {
+    cookiesPopup.style.display = "none";
+  }
+
+  // Comprobar si la cookie ya ha sido aceptada
+  if (document.cookie.indexOf("cookiesAccepted=true") === -1) {
+    cookiesPopup.style.display = "block";
+  } else {
+    cookiesPopup.style.display = "none"; // Ocultar el mensaje si la cookie ya está aceptada
+  }
+
+  // Manejar el clic en el botón de aceptar
+  const acceptButton = document.getElementById("acceptCookies");
+  acceptButton.addEventListener("click", function () {
+    acceptCookies();
+  });
+
+  // Manejar el clic en el botón de cerrar
+  const closeCookiesButton = document.getElementById("closeCookies");
+  closeCookiesButton.addEventListener("click", function () {
+    closeCookies();
+  });
+});
+
